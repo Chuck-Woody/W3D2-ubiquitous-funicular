87f805370047c0c2e2c83e3d4a0ab3322be2fc0c
chuck <you@example.com>
2022-03-08 16:21:53 -0800

    make_guess

--- game.rb
+++ game.rb
@@ -6,7 +6,7 @@ class Game
 
     def initialize
         @board = Board.new
-
+        @previous_guess = []
 
     end
 
@@ -17,8 +17,42 @@ class Game
         [input[0].to_i, input[1].to_i]
     end
 
+    def make_guess(pos)
+
+        x,y = pos
+
+        if @previous_guess.length < 1
+
+            self.board.grid[x][y].turn_over
+            self.board.render
+            @previous_guess << pos
+
+        else
+            card1 = self.board.grid[x][y]
+            card2 = self.board.grid[@previous_guess[0][0]][@previous_guess[0][1]]
+            if  card1 == card2
+                card1.turn_over
+                card2.turn_over
+                @previous_guess.pop
+            else
+                card1.hide
+                card2.hide
+                @previous_guess.pop
+            end
+        end
+
+    end
+
     def play
-        
+
+        @board.render
+
+        guest_pos = self.get_pos
+
+        # @previous_guess << guest_pos
+
+        make_guess(@guest_pos)
+
 
     end
 
